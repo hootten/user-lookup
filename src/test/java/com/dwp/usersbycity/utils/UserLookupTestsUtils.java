eb45e825ec205035498f538fd28152377afb8c1f
Niall Wootten <niallwootten@macbook-pro.home>
2020-06-30 01:52:56 +0100

    test utils

---
+++ src/test/java/com/dwp/usersbycity/utils/UserLookupTestsUtils.java
@@ -0,0 +1,35 @@
+package com.dwp.usersbycity.utils;
+
+import com.dwp.usersbycity.models.User;
+
+public class UserLookupTestsUtils {
+
+    public final static double CITY_LAT = 51.5072;
+    public final static double CITY_LON = -0.1275;
+    public final static int RADIUS = 50;
+    public final static double MILES_PER_METRE = 0.0006213712;
+
+    public static User getUserLivingInCity() {
+        User userLivingInCity = new User();
+        userLivingInCity.setId("1");
+        userLivingInCity.setLatitude("1");
+        userLivingInCity.setLongitude("-3");
+        return userLivingInCity;
+    }
+
+    public static User getUserInCity() {
+        User userInCity = new User();
+        userInCity.setId("2");
+        userInCity.setLatitude("51.7");
+        userInCity.setLongitude("-0.132");
+        return userInCity;
+    }
+
+    public static User getUserNotLinkedToCity() {
+        User userNotLinkedToCity = new User();
+        userNotLinkedToCity.setId("3");
+        userNotLinkedToCity.setLatitude("34");
+        userNotLinkedToCity.setLongitude("4");
+        return userNotLinkedToCity;
+    }
+}
